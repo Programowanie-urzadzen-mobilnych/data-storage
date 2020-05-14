706389181f90390eec7793f3b77575ce43cfec7f
marcinserafin5 <36891799+marcinserafin5@users.noreply.github.com>
2020-05-14 11:21:50 +0200

    feature/SR-57 Data Store API (#2)
    * feature/SR-57 Data Store API
    
    * feature/SR-57 Review Fixes

---
+++ src/main/java/pl/grupa33inf/ssi/data_store/ILogDatastoreImpl.java
@@ -0,0 +1,15 @@
+package pl.grupa33inf.ssi.data_store;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import pl.grupa33inf.ssi.data_store.api.ILogDatastore;
+import pl.grupa33inf.ssi.data_store.api.LogEntry;
+import pl.grupa33inf.ssi.data_store.api.LogSaveResult;
+
+class ILogDatastoreImpl implements ILogDatastore {
+    @Override
+    public <T extends LogEntry> LogSaveResult<T> save(T entry) {
+        return null;
+    }
+}
---
+++ src/main/java/pl/grupa33inf/ssi/data_store/INodeDataStoreImpl.java
@@ -0,0 +1,43 @@
+package pl.grupa33inf.ssi.data_store;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import pl.grupa33inf.ssi.data_store.api.INodeDataStore;
+import pl.grupa33inf.ssi.data_store.api.NodeVariable;
+import pl.grupa33inf.ssi.data_store.api.Result;
+
+class INodeDataStoreImpl implements INodeDataStore {
+
+    @Override
+    public NodeVariable readVariable(UUID deviceUUID, String variableName) {
+        return null;
+    }
+
+    @Override
+    public List<NodeVariable> readAllVariables(UUID deviceUUID) {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public NodeVariable readVariable(UUID deviceUUID, String variableName, LocalDateTime from, LocalDateTime to) {
+        return null;
+    }
+
+    @Override
+    public List<NodeVariable> readAllVariables(UUID deviceUUID, LocalDateTime from, LocalDateTime to) {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public Result writeVariable(UUID deviceUUID, String variableName, String value) {
+        return Result.OK;
+    }
+
+    @Override
+    public Result deleteVariable(UUID deviceUUID, String variableName) {
+        return Result.OK;
+    }
+}

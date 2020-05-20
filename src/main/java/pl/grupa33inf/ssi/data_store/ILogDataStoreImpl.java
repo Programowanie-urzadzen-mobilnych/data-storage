6b5e49d54f2ec9bc82575e86bf924683e1afb216
Przemek Dragańczuk <admin@draganczuk.tk>
2020-05-20 15:00:08 +0200

    Feature/SR-90 Move data store api to submodule

--- src/main/java/pl/grupa33inf/ssi/data_store/ILogDataStoreImpl.java
+++ src/main/java/pl/grupa33inf/ssi/data_store/ILogDataStoreImpl.java
@@ -1,15 +1,17 @@
 package pl.grupa33inf.ssi.data_store;
 
-import android.content.Context;
-import android.widget.Toast;
+import android.util.Log;
 
+import lombok.NoArgsConstructor;
 import pl.grupa33inf.ssi.data_store.api.ILogDatastore;
 import pl.grupa33inf.ssi.data_store.api.LogEntry;
 import pl.grupa33inf.ssi.data_store.api.LogSaveResult;
 
-class ILogDatastoreImpl implements ILogDatastore {
+@NoArgsConstructor
+public class ILogDataStoreImpl implements ILogDatastore {
     @Override
     public <T extends LogEntry> LogSaveResult<T> save(T entry) {
+        Log.d("ILogDataStore", entry.toString());
         return null;
     }
 }

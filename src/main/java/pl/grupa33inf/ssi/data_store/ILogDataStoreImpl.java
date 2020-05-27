916e54d3163e3507f0251539e0d08f2f70516148
Aksel <aksel.stankiewicz@onet.eu>
2020-05-27 09:26:56 +0200

    feature/SR-59

--- src/main/java/pl/grupa33inf/ssi/data_store/ILogDataStoreImpl.java
+++ src/main/java/pl/grupa33inf/ssi/data_store/ILogDataStoreImpl.java
@@ -1,17 +1,17 @@
 package pl.grupa33inf.ssi.data_store;
 
-import android.util.Log;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import lombok.NoArgsConstructor;
 import pl.grupa33inf.ssi.data_store.api.ILogDatastore;
 import pl.grupa33inf.ssi.data_store.api.LogEntry;
-import pl.grupa33inf.ssi.data_store.api.LogSaveResult;
 
 @NoArgsConstructor
 public class ILogDataStoreImpl implements ILogDatastore {
     @Override
-    public <T extends LogEntry> LogSaveResult<T> save(T entry) {
-        Log.d("ILogDataStore", entry.toString());
-        return null;
+    public void save(LogEntry entry) {
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("logs").push().setValue(entry);
     }
 }
--- src/main/java/pl/grupa33inf/ssi/data_store/INodeDataStoreImpl.java
+++ src/main/java/pl/grupa33inf/ssi/data_store/INodeDataStoreImpl.java
@@ -2,13 +2,18 @@ package pl.grupa33inf.ssi.data_store;
 
 import android.util.Log;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
 import lombok.NoArgsConstructor;
 import pl.grupa33inf.ssi.data_store.api.INodeDataStore;
+import pl.grupa33inf.ssi.data_store.api.NodeValue;
 import pl.grupa33inf.ssi.data_store.api.NodeVariable;
 import pl.grupa33inf.ssi.data_store.api.Result;
 
@@ -17,6 +22,9 @@ public class INodeDataStoreImpl implements INodeDataStore {
 
     @Override
     public NodeVariable readVariable(UUID deviceUUID, String variableName) {
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+
+
         return null;
     }
 
@@ -37,12 +45,24 @@ public class INodeDataStoreImpl implements INodeDataStore {
 
     @Override
     public Result writeVariable(UUID deviceUUID, String variableName, String value) {
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference variable = mDatabase.child("nodes").child(deviceUUID.toString()).child(variableName);
+        variable.child("value").setValue(value);
+        variable.child("history").push().setValue(NodeValue.builder()
+                .value(value)
+                .timestamp(new Date())
+                .build()
+        );
+
         Log.d("INodeDataStore", String.format("%s, %s, %s", deviceUUID.toString(), variableName, value));
         return Result.OK;
     }
 
     @Override
     public Result deleteVariable(UUID deviceUUID, String variableName) {
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference variable = mDatabase.child("nodes").child(deviceUUID.toString()).child(variableName);
+        variable.child("deleted").setValue(true);
         return Result.OK;
     }
 }
